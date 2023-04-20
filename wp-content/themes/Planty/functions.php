08f3600cf8196bae8d89b8ef58363479f851e232
Marius Carre <marius.carre@yahoo.fr>
2023-04-20 17:44:55 +0200

    Shortcodes et fonctions

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -4,9 +4,48 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+    wp_enqueue_style('banniere-titre-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/banniere-titre.css', array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/banniere-titre.css'));
+
 }
 function register_my_menu(){
   register_nav_menu( 'main-menu', 'Menu principal' );
 }
 add_action( 'after_setup_theme', 'register_my_menu' );
+
+
+
+
+/* SHORTCODES */
+
+add_shortcode('banniere-titre', 'banniere_titre_func');
+
+function banniere_titre_func($atts)
+{
+    
+    $atts = shortcode_atts(array(
+        'src' => '',
+        'titre' => 'Titre'
+    ), $atts, 'banniere-titre');
+
+    
+    ob_start();
+
+    if ($atts['src'] != "") {
+        ?>
+
+        <div class="banniere-titre" style="background-image: url(<?= $atts['src'] ?>)">
+            <h2 class="titre"><?= $atts['titre'] ?></h2>
+        </div>
+
+        <?php
+    }
+
+    
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
+}
+
 ?>
